8c416f616c2c312d756a8723650c56d795a1e900
Simon Wallner <me@simonwallner.at>
2014-08-13 10:59:23 +0200

    dropped in unity project

---
+++ Unity/telemetry-test/Assets/scripts/Init.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Init : MonoBehaviour {
+
+	public Camera mainCamera;
+
+	void Awake () {
+		Debug.Log("init....");
+		Telemetry.trace("hello!");
+
+		Constants.Init();
+
+		Application.runInBackground = true;
+	}
+}
